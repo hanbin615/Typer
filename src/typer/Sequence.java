d5b55cc9d7482d82ced255604a1bde012f2bb4ad
happy <hanbin615@hotmail.com>
2018-12-14 11:41:16 +0100

    temp

--- src/typer/Sequence.java
+++ src/typer/Sequence.java
@@ -16,7 +16,7 @@ import javafx.scene.layout.Pane;
  * @author happy
  */
 public class Sequence {
-    public ArrayList<Tile> list = new ArrayList();
+    public ArrayList<AbstractTile> list = new ArrayList();
     public Pane pane;
     public StringProperty text;
     
@@ -29,7 +29,7 @@ public class Sequence {
     public int index = 0;
     public void next(){
         if (index < list.size()){
-            Tile t = list.get(index++);
+            AbstractTile t = list.get(index++);
 //            System.out.println("placing tile " +t.getText() +" at x= " +pane.getWidth() +", y= " +pane.getHeight());
             t.setLayoutX(pane.getWidth());
             t.setLayoutY((pane.getHeight()+t.heightProperty().get())/2);
@@ -44,16 +44,16 @@ public class Sequence {
     
     public void load(){
         // TODO
-        list.add(new Tile("start", text));// TODO debug, why it start at x=0 y=0?
-        list.add(new Tile("my", text, 2000));
-        list.add(new Tile("name", text));
-        list.add(new Tile("is", text));
-        list.add(new Tile("max", text));
+        list.add(new AbstractTile("start", text));// TODO debug, why it start at x=0 y=0?
+        list.add(new AbstractTile("my", text, 2000));
+        list.add(new AbstractTile("name", text));
+        list.add(new AbstractTile("is", text));
+        list.add(new AbstractTile("max", text));
     }
     
     public void start(){
         if (pane != null){
-            pane.getChildren().add(new Tile("hello world", text));
+            pane.getChildren().add(new AbstractTile("hello world", text));
             load();
             next();
         }
